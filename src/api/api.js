ba1bae96685b4763c8c5f0ff65aa3bce42e622ca
alekspot <49140637+alekspot@users.noreply.github.com>
2019-10-22 09:32:08 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,4 @@
 import * as axios from "axios";
-import { async } from "q";
-
 const apiKey = '4a1a49b2bd67384870a57039fbe6ff33';
 
 const instance = axios.create({
@@ -9,9 +7,6 @@ const instance = axios.create({
 
 const instance2 = axios.create({
     baseURL: `https://api.themoviedb.org/3/`,
-    baasseURL: `
-              https://api.themoviedb.org/3/tv/94521/images?api_key=4a1a49b2bd67384870a57039fbe6ff33
-    `
 });
 
 
@@ -34,11 +29,8 @@ export const API = {
     async getImages (id, type) {
         const images = await instance.get(`${type}/${id}/images?api_key=4a1a49b2bd67384870a57039fbe6ff33`);
         console.log(images.data.posters);
-        return images.data.posters.map(item => (`https://image.tmdb.org/t/p/original${item.file_path}`));
-
-      
+        return images.data.posters.map(item => (`https://image.tmdb.org/t/p/original${item.file_path}`)); 
     }
-
 }
 
 
